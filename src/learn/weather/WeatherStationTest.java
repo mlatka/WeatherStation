f5626fc46d759bef217f880484fbd34ed0d5290e
mirek <mlatka@gmail.com>
2018-07-22 19:18:56 +0200

    JSON parsing with JSONsimple

--- src/learn/weather/WeatherStationTest.java
+++ src/learn/weather/WeatherStationTest.java
@@ -1,5 +1,12 @@
 package learn.weather;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
 public class WeatherStationTest {
 
 
@@ -9,8 +16,42 @@ public class WeatherStationTest {
 
 
         String result=myStation.getOpenWeatherData("Wroclaw");
-
         System.out.println(result);
+
+        //JSONObject obj = new JSONObject();
+        JSONParser parser= new JSONParser();
+
+        try {
+            Object obj= parser.parse(result);
+            System.out.println("\n" + obj + "\n");
+            JSONObject json= (JSONObject) obj;
+            System.out.println(((JSONObject)json.get("main")).get("temp"));
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+//        JSONObject obj = new JSONObject();
+//
+//        obj.put("name","foo");
+//        obj.put("num",100);
+//        obj.put("balance",1000.21);
+//        obj.put("is_vip",true);
+//
+//        StringWriter out = new StringWriter();
+//        try {
+//            obj.writeJSONString(out);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        String jsonText = out.toString();
+//        System.out.print(jsonText);
+
 
     }
 
